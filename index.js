fe7d4a3366e9670419878e0f399755a4ae9a02b1
shamimspro <shamimspro@gmail.com>
2022-11-30 00:29:09 +0600

    create index file and example server ready

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>Shamim sarker server is working fine.....</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
