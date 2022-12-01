d25e36476133296b4b22449ac58ee367b1c42b4c
shamimspro <shamimspro@gmail.com>
2022-12-01 16:44:39 +0600

    send data to db

--- index.js
+++ index.js
@@ -1,11 +1,124 @@
 const express = require('express');
+const cors = require('cors');
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+
+    const categoriesCollection = client.db('shamimSarker').collection("categories");
+    const notesCollection = client.db('shamimSarker').collection("notes");
+
+    // Add a category
+    app.post('/categories', async (req, res) => {
+      const category = req.body;
+      console.log(category);
+      const result = await categoriesCollection.insertOne(category);
+      res.send(result);
+      console.log('Category is added successfully!');
+    });
+
+    // Get all categories
+    app.get('/categories', async (req, res) => {
+      const query = {};
+      const result = await categoriesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Get all categories
+    app.get('/categories/:category', async (req, res) => {
+      const category = req.params.category;
+      const query = {category: category};
+      const result = await categoriesCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Add a note
+    app.post('/notes', async (req, res) => {
+      const note = req.body;
+      console.log(note);
+      const result = await notesCollection.insertOne(note);
+      res.send(result);
+      console.log('Note is added successfully!');
+    });
+
+
+
+    /*
+        // R from CRUD
+        app.get('/users', async (req, res) => {
+          const query = {};
+          const cursor = collection.find(query);
+          const users = await cursor.toArray();
+          res.send(users);
+        });
+
+        // R from CRUD (find by id)
+        app.get('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const user = await collection.findOne(query);
+          res.send(user);
+        });
+
+        // R from CRUD (using query parameters)
+        // http://localhost:5000/usrs?firstName=Shamim (query parameter format)
+        app.get('/usrs', async (req, res) => {
+          let query = {};
+          if(req.query.firstName) {
+            query = {
+              firstName: req.query.firstName
+            };
+          }
+          const cursor = collection.find(query);
+          const usrs = await cursor.toArray();
+          res.send(usrs);
+        });
+
+        // U from CRUD
+        app.put('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = {_id: ObjectId(id)};
+          const user = req.body;
+          const option = {upsert: true};
+          const updatedUser = {
+            $set: {
+              firstName: user.firstName,
+              lastName: user.lastName
+            }
+          };
+          const result = await collection.updateOne(filter, updatedUser, option);
+          res.send(result);
+        });
+
+        // D from CRUD
+        app.delete('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const result = await collection.deleteOne(query);
+          res.send(result);
+        }); */
+
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('<h1>Shamim sarker server is working fine.....</h1>');
+  res.send('Server is working fine!!!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
