defc6b31f9de2f89eb67bddda1300bc806e42e9d
shamimspro <shamimspro@gmail.com>
2022-12-03 00:12:06 +0600

    connect mongodb atlus

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
+// Local Database
+// const uri = 'mongodb://localhost:27017';
+// const client = new MongoClient(uri);
+
+// Mongodb Atlas
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egsefuu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
 async function run() {
   try {
@@ -24,6 +29,21 @@ async function run() {
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Get all users
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Get user by email
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email};
+      const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
